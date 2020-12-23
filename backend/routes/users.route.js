afb7b6bb473838717b0fa9b14165efe2bc4460d1
Minh Nguyen Hoang Duong <johncegom203@gmail.com>
2020-12-24 00:45:59 +0700

    Add basic API (get/post) for user.

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -2,8 +2,31 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log("wait");
+// Import model
+const userModel = require('../models/user.model');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await userModel.find();
+        res.json(users);
+    } catch(err) {
+        res.json({message: err});
+    }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const user = new userModel( {
+            name: req.body.name,
+            description: req.body.description
+        });
+
+        const savedUser = await user.save();
+        res.json(savedUser);
+    } catch(err) {
+        res.json({message: err});
+    }
+})
 
 module.exports = router;
